fad77b765c4af173b683872c606a3349d4d3a78e
rubydawnstrider@yahoo.com <rubydawnstrider@users.noreply.github.com>
2020-06-26 04:18:53 -0500

    barebones is there...

--- bot.py
+++ bot.py
@@ -5,84 +5,150 @@ from discord.ext import commands
 import pymongo
 from pymongo import MongoClient
 
-#from dotenv import load_dotenv
-#load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
-url_conn = os.environ['MONGO_CONN'] #os.getenv('CONN')
+url_conn = os.getenv('CONN')#os.environ['MONGO_CONN'] #
 cluster = MongoClient(url_conn)
 db = cluster['handy-dandy-helper-mofo']
 collection = db['config-data']
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command()
-async def set_repeat_channel(ctx, arg):
+
+@bot.command(pass_context=True)
+async def set_repeat_ch(ctx, arg:discord.TextChannel):
     if ctx.guild is None:
         await ctx.channel.send('this is only designed to run on a server')
         return
-    if arg is None or arg == '':
-        await ctx.channel.send('huh? you need to provide a channel name')
+    if arg is None:
+        await ctx.channel.send('huh? you need to provide a channel')
         return
 
-    channel_id = None
-    for channel in ctx.guild.channels:
-        if channel.name == arg:
-            channel_id = str(channel.id)
-            break
-
-    if channel_id is None:
-        await ctx.channel.send('you need to provide a valid channel name')
-        return
-        
     #server should have been added in setup so we'll assume its available to update
-    collection.update_one({'_id': str(ctx.guild.id)}, {'$set':{'repeat_channel': channel_id}})
-    await ctx.channel.send(f'repeat channel saved as {arg}')
+    collection.update_one({'_id': str(ctx.guild.id)}, {'$set':{'repeat_channel': arg.id}})
+    await ctx.channel.send(f'repeat channel saved as {arg.mention}')
+
+
+@bot.command(pass_context=True)
+async def set_repeat_role(ctx, arg:discord.Role):
+    if ctx.guild is None:
+        await ctx.channel.send('this is only designed to run on a server')
+        return
+    if arg is None:
+        await ctx.channel.send('huh? you need to provide a role')
+        return
+
+    #server should have been added in setup so we'll assume its available to update
+    collection.update_one({'_id': str(ctx.guild.id)}, {'$set':{'repeat_role': arg.id}})
+    await ctx.channel.send(f'repeat role saved as {arg.mention}')
     
 
-@bot.command()
-async def repeat_channel(ctx):
+@bot.command(pass_context=True)
+async def repeat_ch(ctx):
     if ctx.guild is None:
         await ctx.channel.send('this is only designed to run on a server')
         return
     
-    query = {'_id': str(ctx.guild.id)}
+    query = {'_id': str(ctx.guild.id), 'repeat_channel':{'$exists':True,'$ne':None} }
     guild = collection.find_one(query)
-    if quild is None:
+    if guild is None:
         await ctx.channel.send('no repeat channel is saved')
         return
 
-    channel = guild['repeat_channel']
-    await ctx.channel.send(f'repeat channel is currently {channel}')
+    channel = ctx.guild.get_channel(guild['repeat_channel'])
+    if channel is not None:
+        await ctx.channel.send(f'repeat channel is currently {channel.mention}')
+    else:
+        await ctx.channel.send(f'repeat channel is set but could not be found on the server :o')
 
 
-@bot.command()
+@bot.command(pass_context=True)
+async def repeat_role(ctx):
+    if ctx.guild is None:
+        await ctx.channel.send('this is only designed to run on a server')
+        return
+    
+    query = {'_id': str(ctx.guild.id), 'repeat_role':{'$exists':True,'$ne':None} }
+    guild = collection.find_one(query)
+    if guild is None:
+        await ctx.channel.send('no repeat role is saved')
+        return
+
+    role = ctx.guild.get_role(guild['repeat_role'])
+    if role is not None:
+        await ctx.channel.send(f'repeat role is currently {role.name}')
+    else:
+        await ctx.channel.send(f'repeat role is set but could not be found on the server :o')
+
+
+@bot.command(pass_context=True)
 async def boop(ctx):
-    print('BOOP')
-    await ctx.channel.send(f'**BOOP, {ctx.message.author.mention()}!**')
+    author = ctx.message.author.mention
+    await ctx.channel.send(f'**BOOP, {author}!**')
 
     
 @bot.event
 async def on_ready():
-    print(f'mongo connection to \'{url_conn}\'')
     print(f'{bot.user} has connected to Discord!')
 
     for guild in bot.guilds:
         print(f'running on server: {guild.name} {guild.id}')
         # if we didnt add a setting for the server yet, add it
-        if collection.count_documents({'_id': str(guild.id) }) == 0:
+        query = {'_id': str(guild.id)}
+        g0 = collection.find_one(query)
+        if g0 is None:
             print(f'adding server: {guild.name} {guild.id} to the DB . . .')
             server = {'_id': str(guild.id), 'guild_name': guild.name}
             collection.insert_one(server)
 
 
-##@bot.event
-##async def on_message(message):
-##    if message.author == bot.user:
-##        return
-##
-##    print(f'msg:~{message.content} | {message.type}~')
+@bot.event
+async def on_member_join(ctx):
+    query = {'_id': str(ctx.guild.id)}
+    guild = collection.find_one(query)
+    defaultChannel = ctx.guild.system_channel
+    repeatChannel = ctx.guild.get_channel(guild['repeat_channel'])
+    repeatRole = ctx.guild.get_role(guild['repeat_role'])
+
+#    print(f'join: {ctx}')
+#    await defaultChannel.send(f'repeat channel: {repeatChannel.mention}')
+#    await defaultChannel.send(f'repeat role: {repeatRole.mention}')
+
+    if repeatChannel is not None:
+        if repeatRole is not None:
+            await repeatChannel.send(f'Yo, {repeatRole.mention}. Newb alert: {ctx.mention}! Better make sure they got the right nickname and role')
+        else:
+            await repeatChannel.send(f'Welcome to the mayhem, {ctx.mention}!')
+    
+
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+
+    #dont process here if this is a command for the bot
+    if ctx.content.startswith(bot.command_prefix):
+        await bot.process_commands(ctx)
+        return
+
+    if ctx.type == discord.MessageType.new_member:
+        return
+    
+    print(f'msg:~{ctx.content} | {ctx.type}~')
 ##    new_msg = 'repeat: ' + message.content
 ##    await message.channel.send(new_msg)
 
-token = os.environ['DISCORD_TOKEN'] #os.getenv('TOKEN')
+token = os.getenv('TOKEN') #os.environ['DISCORD_TOKEN'] #
 bot.run(token)
+
+
+#######################################################
+## TODO
+## - store all unhandled newbs in DB
+## - cmd: set nickname for newb
+## - - make it remove the newb from teh db list
+## - cmd: get list of unhandled newbs
+## - cmd: remove newb from list
+## - cmd: purge newb list
+
